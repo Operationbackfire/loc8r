39761a1da897ebf1e302c03983410916cbd46f8f
Operationbackfire <clausmt@gmail.com>
2018-01-24 22:08:33 +0100

    added what was missing to models/locations.js and controllers/locations.js, mongodumped to mLab

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -8,6 +8,7 @@ module.exports.homelist = function(req, res){
 			strapline: 'Find places to work with wifi near you!'
 		},
 		sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+		
 		locations: [
 			{
 	         name: 'Starcups',
@@ -36,7 +37,42 @@ module.exports.homelist = function(req, res){
 
 /* GET 'Location info' page */
 module.exports.locationInfo = function(req, res){
-	res.render('location-info', { title: 'Location info' });
+	res.render('location-info', { 
+	
+		location: {
+		  name: 'Starcups',
+		  address: '125 High Street, Reading, RG6 1PS',
+		  rating: 3,
+		  facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+		  coords: {lat: 51.455041, lng: -0.9690884},
+		  openingTimes: [{
+		    days: 'Monday - Friday',
+		    opening: '7:00am',
+		    closing: '7:00pm',
+		    closed: false
+		  },{
+		    days: 'Saturday', 
+			opening: '8:00am',
+			closing: '5:00pm',
+			closed: false 
+		  },{
+			days: 'Sunday',
+			closed: true 
+		  }],
+		  reviews: [{
+		  	author: 'Simon Holmes',
+		  	rating: 5,
+		  	timestamp: '16 July 2013',
+		  	reviewText: 'What a great place. I can\'t say enough good things about it.'
+		  },{
+		  	author: 'Charlie Chaplin',
+		  	rating: 3,
+		  	timestamp: '16 June 2013',
+		  	reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
+		  }] 
+		}
+	
+	});
 };
 
 /* GET 'Add review' page */
